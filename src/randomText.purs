e001b112ce3b0878253beb7828f69572bb22c354
Alexandre Delanoë <devel+git@delanoe.org>
2017-11-29 17:28:38 +0100

    [Compiling, ok] need to rename function and map on sentence and paragraph.

--- src/randomText.purs
+++ src/randomText.purs
@@ -14,11 +14,16 @@ import Partial.Unsafe (unsafePartial)
 import Unsafe.Coerce (unsafeCoerce)
 
 
---rando Ran x [] = Ran x []
---rando Ran x xs = Ran (x <> [x']) (rando xs')
---    where
---        Ran x' xs' = randomIt xs
---
+data RanR = RanR { l :: Array Char, r :: Array Char}
+
+instance showRanR :: Show RanR where
+    show (RanR {l:l', r:r'}) = show $ (show l') /\ (show r')
+
+rando (RanR {l:x,r:[]}) = pure $ RanR {l:x,r:[]}
+rando (RanR {l:x,r:xs}) = do
+    Ran {l:x',r:xs'} <- randomIt xs
+    rando (RanR {l:(x <> [x']), r: xs'})
+
 
 remove :: forall t5. Int -> Array t5 -> Array t5
 remove n [] = []
@@ -29,29 +34,44 @@ remove n xs = unsafePartial $ case n of
 
 data Ran = Ran { l :: Char, r :: Array Char}
 
-randomIt :: forall t46. String -> Eff ( random :: RANDOM | t46 ) Ran
-randomIt ar = unsafePartial $ do
-    let ar' = toCharArray ar
-    n    <- randomInt 0 (length ar' - 1)
+instance showRan :: Show Ran where
+    show (Ran {l:l', r:r'}) = show $ (show l') /\ (show r')
 
-    let maybeChar = (ar' !! n )
-    let rest   = remove n ar'
+randomIt :: forall t46. Array Char -> Eff ( random :: RANDOM | t46 ) Ran
+randomIt ar = unsafePartial $ do
+    -- let ar' = toCharArray ar
+    n    <- randomInt 0 (length ar - 1)
+
+    let maybeChar = (ar !! n )
+    let rest   = remove n ar
 
     case maybeChar of
-         Nothing    ->
-           crash "it should not happen"
-         Just char  ->
-           pure $ Ran {l : char, r : rest}
+         Nothing    -> 
+            crash "it should not happen"
+         Just char  -> 
+            pure $ Ran {l : char, r : rest}
 
-randomText :: String -> String
-randomText txt = fromCharArray ( start <> middle <> end)
+randomize :: forall t98. String -> Eff ( random :: RANDOM | t98) String
+randomize string = do 
+    RanR rr <- rando (RanR {l:[], r:(toCharArray string)})
+    pure $ fromCharArray (rr.l)
+
+
+randomize' :: forall t98. (Array Char) -> Eff ( random :: RANDOM | t98) (Array Char)
+randomize' string = do 
+    RanR rr <- rando (RanR {l:[], r:string})
+    pure rr.l
+
+
+randomText :: forall t114. String -> Eff( random :: RANDOM| t114) String
+randomText txt = randomize' middle >>= \middle' -> pure $ fromCharArray ( start <> middle' <> end)
         where
-            txt'   = toCharArray        txt
-            start  = take 2             txt'
-            middle = dropEnd 2 $ drop 2 txt'
-            end    = takeEnd 2          txt'
+            txt'    = toCharArray        txt
+            start   = take 2             txt'
+            middle  = dropEnd 2 $ drop 2 txt'
+            end     = takeEnd 2          txt'
 
-testText :: String -> String
+testText :: forall t114. String -> Eff( random :: RANDOM| t114) String
 testText txt = case (length (toCharArray txt)) >= 5 of
                     true -> randomText txt
-                    _    -> txt
+                    _    -> pure txt
