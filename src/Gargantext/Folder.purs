15ee6d9cf676f2697448e0a9471c5e8ac73cc4dc
Alexandre Delanoë <devel+git@delanoe.org>
2018-09-25 22:39:04 +0200

    [Folder] Text description of the folder.

--- src/Gargantext/Folder.purs
+++ src/Gargantext/Folder.purs
@@ -1,11 +1,15 @@
 module Gargantext.Folder where
 
 import Prelude
-import React.DOM (div, h1, text)
+import React.DOM (div, h1, text, p)
 import Thermite (Render, Spec, simpleSpec, defaultPerformAction)
 
+-- TODO : get REST informations
+
 render :: Render {} {} Void
-render dispatch _ state _ = [h1 [] [text "Folder"]]
+render dispatch _ state _ = [ h1 [] [text "Folder"] 
+                            , text "Some description of the folder here"
+                            ]
 
 layoutFolder :: Spec {} {} Void
 layoutFolder = simpleSpec defaultPerformAction render
