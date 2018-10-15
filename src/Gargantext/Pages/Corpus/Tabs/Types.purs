081e7de01a2a499916979e9d3ab8189acfd7cf33
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2018-10-15 15:41:57 +0200

    Add missing file: src/Gargantext/Pages/Corpus/Tabs/Types.purs

---
+++ src/Gargantext/Pages/Corpus/Tabs/Types.purs
@@ -0,0 +1,6 @@
+module Gargantext.Pages.Corpus.Tabs.Types where
+
+type Props = {nodeId :: Int, totalRecords :: Int}
+
+-- TODO include Gargantext.Pages.Corpus.Tabs.States
+-- TODO include Gargantext.Pages.Corpus.Tabs.Actions
