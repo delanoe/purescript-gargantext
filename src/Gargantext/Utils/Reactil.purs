9181d244cb20daa341aea84876b361adaa3ff920
James Laver <james.laver@gmail.com>
2019-04-09 17:05:18 +0200

    Move Gargantext.Utils.Reactil to Gargantext.Utils.React

--- src/Gargantext/Utils/Reactil.purs
+++
@@ -1,7 +0,0 @@
-module Gargantext.Utils.Reactil where
-
-import Prelude
-import React (ReactElement)
-
-wrap :: (Array ReactElement -> ReactElement) -> ReactElement -> ReactElement
-wrap f e = f [e]
