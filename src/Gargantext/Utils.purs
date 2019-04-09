39e3ef0a812b3c4658e709adc4de541cdcb4a4a3
James Laver <james.laver@gmail.com>
2019-04-09 17:06:35 +0200

    Add invertOrdering and _unit to Gargantext.Utils

--- src/Gargantext/Utils.purs
+++ src/Gargantext/Utils.purs
@@ -1,8 +1,9 @@
 module Gargantext.Utils where
 
 import Prelude
-
+import Data.Ordering (Ordering(..))
 import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Lens (Lens', lens)
 
 setterv :: forall nt record field. Newtype nt record => (record -> field -> record) -> field -> nt -> nt
 setterv fn v t = (setter (flip fn v) t)
@@ -12,3 +13,13 @@ setter fn = wrap <<< fn <<< unwrap
 
 getter :: forall record field nt. Newtype nt record => (record -> field) -> nt -> field
 getter fn = fn <<< unwrap
+
+-- Default sort order is ascending, we may want descending
+invertOrdering :: Ordering -> Ordering
+invertOrdering LT = GT
+invertOrdering GT = LT
+invertOrdering EQ = EQ
+
+-- A lens that always returns unit
+_unit :: forall s. Lens' s Unit
+_unit = lens (\_ -> unit) (\s _ -> s)
