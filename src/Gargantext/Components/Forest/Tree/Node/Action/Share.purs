f28f7590e381bd9cc45590fd6ada673d46df5048
Alexandre Delanoë <devel+git@delanoe.org>
2020-07-07 18:39:24 +0200

    [FIX] generics types ShareNode

--- src/Gargantext/Components/Forest/Tree/Node/Action/Share.purs
+++ src/Gargantext/Components/Forest/Tree/Node/Action/Share.purs
@@ -5,20 +5,29 @@ import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Prelude (($))
 import Reactix as R
-import Gargantext.Components.Forest.Tree.Node.Action
+import Gargantext.Components.Forest.Tree.Node.Action (Action)
+import Gargantext.Components.Forest.Tree.Node.Action as Action
 import Gargantext.Types as GT
 import Gargantext.Types (ID)
 import Gargantext.Routes as GR
 import Gargantext.Sessions (Session, post)
 import Gargantext.Components.Forest.Tree.Node.Tools as Tools
 
+
+import Data.Argonaut as Argonaut
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Gargantext.Utils.Argonaut (genericSumDecodeJson, genericSumEncodeJson, genericEnumDecodeJson, genericEnumEncodeJson)
+import Data.Maybe (Maybe(..))
+import Gargantext.Prelude (class Eq, class Read, class Show)
+
 ------------------------------------------------------------------------
-share :: Session -> ID -> ShareValue -> Aff (Array ID)
+share :: Session -> ID -> ShareNode -> Aff ID
 share session nodeId =
   post session $ GR.NodeAPI GT.Node (Just nodeId) "share"
 
 shareAction :: String -> Action
-shareAction username = ShareNode username
+shareAction username = Action.ShareTeam username
 
 ------------------------------------------------------------------------
 newtype ShareValue = ShareValue
@@ -32,4 +41,23 @@ instance encodeJsonShareValue :: EncodeJson ShareValue where
 ------------------------------------------------------------------------
 textInputBox :: Record Tools.TextInputBoxProps -> R.Element
 textInputBox  = Tools.textInputBox
+
+------------------------------------------------------------------------
+
+data ShareNode = ShareTeam { username :: String }
+               | SharePublic { rights :: String }
+
+derive instance eqShareNode :: Eq ShareNode
+
+derive instance genericShareNode :: Generic ShareNode _
+
+instance showShareNode :: Show ShareNode where
+  show = genericShow
+
+instance decodeJsonShareNode :: Argonaut.DecodeJson ShareNode where
+  decodeJson = genericSumDecodeJson
+
+instance encodeJsonShareNode :: Argonaut.EncodeJson ShareNode where
+  encodeJson = genericSumEncodeJson
+
 
