353645dee04fdaf4d0c749eaf31112fa26377930
Alexandre Delanoë <devel+git@delanoe.org>
2020-07-10 14:24:08 +0200

    [CSS] fix design and legal footer

--- src/Gargantext/Components/Nodes/Home/Public.purs
+++ src/Gargantext/Components/Nodes/Home/Public.purs
@@ -3,11 +3,8 @@ module Gargantext.Components.Nodes.Home.Public where
 import Data.String (take)
 import Gargantext.Prelude
 import Data.Tuple.Nested ((/\))
-import Data.Newtype (class Newtype)
-import Effect (Effect)
 import Reactix as R
 import Reactix.DOM.HTML as H
-import Gargantext.Sessions (Session(..))
 
 
 type PublicProps = (publicDatas :: R.State (Array PublicData)
@@ -33,7 +30,18 @@ publicLayoutCpt = R.staticComponent "[G.C.N.H.Public.publicLayout" cpt
   where
     cpt {publicDatas} _ =
       H.span {}
-             [ H.div { className: "container1" } [ H.h2 {} [H.text "Public Maps"]]
+             [ H.div { className: "text-center" }
+                     [ H.div { className:"container1" }
+                             [ H.h2 {} [H.text "Public Maps"]
+                             , H.p {className: "lead text-muted"} [H.text "Discover maps made with "
+                                   , H.div {className: "fa fa-heart"} []]
+                             , H.p { className:"flex-space-around" }
+                                   [ H.a { className: "btn btn-primary my-2"
+                                         , href :"https://gargantext.org"
+                                         } [H.text "Join"]
+                                   ]
+                             ]
+                     ]
              -- | TODO browse maps
              -- | TODO random maps
              , album pds
@@ -41,6 +49,18 @@ publicLayoutCpt = R.staticComponent "[G.C.N.H.Public.publicLayout" cpt
         where
           (pds /\ _setPublicData) = publicDatas
 
+{-
+   <section class="jumbotron text-center">
+      <div class="container" >
+        <h1 class="jumbotron-heading">Gargantext Maps</h1>
+        <p class="lead text-muted">Discover maps made with love</p>
+        <p>
+          <a href="http://gargantext.org" target="blank" class="btn btn-primary my-2">More about Gargantext</a>
+          <a href="http://iscpif.fr" target="blank"  class="btn btn-secondary my-2">CNRS/ISC-PIF</a>
+        </p>
+      </div>
+    </section>
+    -}
 album :: Array PublicData -> R.Element
 album pd = H.div {className: "album py-5 bg-light"}
                  [ H.div { className: "container" }
--- src/Gargantext/License.purs
+++ src/Gargantext/License.purs
@@ -1,26 +1,23 @@
 module Gargantext.License where
 
-import Prelude
 import Reactix as R
 import Reactix.DOM.HTML as H
 
 license :: R.Element
 license = H.p {}
             [ H.text "Gargantext "
-            , H.span {className: "glyphicon glyphicon-registration-mark"} []
-            , H.a { href: "http://www.cnrs.fr"
+            , H.span { className: "glyphicon glyphicon-registration-mark"} []
+            , H.text " is made by "
+            , H.a { href: "https://iscpif.fr"
                   , target: "blank"
-                  , title: "Project hosted by CNRS."
-                  }
-              [ H.text ", Copyrights "
-              , H.span { className: "glyphicon glyphicon-copyright-mark" } []
-              , H.text " CNRS 2017-Present"
-              ]
+                  } [ H.text "CNRS/ISCPIF" ]
             , H.a { href: "http://gitlab.iscpif.fr/humanities/gargantext/blob/stable/LICENSE"
                   , target: "blank"
                   , title: "Legal instructions of the project."
                   }
-              [ H.text ", Licences aGPLV3 and CECILL variant Affero compliant" ]
+              [ H.text ", with licences aGPLV3 and CECILL variant Affero compliant, " ]
+            , H.span { className: "glyphicon glyphicon-copyright-mark"   } []
+            , H.a { href: "https://cnrs.fr", target:"blank"} [H.text " CNRS 2017-Present "]
             , H.text "."
             ]
 
