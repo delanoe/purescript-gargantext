9066a0bf7cadcba957a383d27824de8f2974607a
Alexandre Delanoë <devel+git@delanoe.org>
2020-10-08 14:44:07 +0200

    [FIX] perf issue with List related with #218 (thx @np)

--- src/Gargantext/Utils/List.purs
+++ src/Gargantext/Utils/List.purs
@@ -1,9 +1,11 @@
 module Gargantext.Utils.List where
 
 import Data.Ord (class Ord, comparing)
-import Data.List as List
+import Data.List  as List
+import Data.Array as Array
+import Gargantext.Prelude
 
 -- same as
 -- https://github.com/purescript/purescript-arrays/blob/v5.3.1/src/Data/Array.purs#L715-L715
 sortWith :: forall a b. Ord b => (a -> b) -> List.List a -> List.List a
-sortWith f = List.sortBy (comparing f)
+sortWith f l = Array.toUnfoldable $ Array.sortBy (comparing f) $ Array.fromFoldable l
