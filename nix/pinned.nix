63d4d9c973dd3571426990ce6aa9b4ef5fd5587a
Przemek Kaminski <pk@intrepidus.pl>
2021-01-13 15:34:40 +0100

    [nix] upgrade sha256 for nix/easy-ps.nix and nix/pinned.nix

--- nix/pinned.nix
+++ nix/pinned.nix
@@ -1,6 +1,6 @@
 import (
   builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
-    sha256 = "07n91k3d9i9pym8npsszha9mnvg4d1r0l0ldnhk4g8sx15vv1br5";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
   }
 )
