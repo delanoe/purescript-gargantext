dda4ade025d92906a3de0f19f304c93c1fc24d4b
Przemek Kaminski <pk@intrepidus.pl>
2021-02-25 08:31:56 +0100

    [nix] update sha256 in easy-ps

--- nix/easy-ps.nix
+++ nix/easy-ps.nix
@@ -6,7 +6,7 @@ import
       owner = "justinwoo";
       repo = "easy-purescript-nix";
       rev = "c8c32741bc09e2ac0a94d5140cf51fa5de809e24";
-      sha256 = "1g1hlybld298kimd1varvwiflpb0k7sdqlmcqha3kswjvy5z4k6k";
+      sha256 = "0rn938nbxqsd7lp7l8z1y7bhzaq29vbziq6hq9llb3yh9xs10lmf";
     }
   ) {
   inherit pkgs;
