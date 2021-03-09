0a995acfb3d1fc6b92e6f6d7a21b548abd5f86bc
jjl <james.laver@gmail.com>
2021-03-09 10:02:48 +0100

    blah

--- src/Gargantext/Components/App.purs
+++ src/Gargantext/Components/App.purs
@@ -21,9 +21,8 @@ app = R.createElement appCpt {} []
 appCpt :: R.Component ()
 appCpt = here.component "app" cpt where
   cpt _ _ = do
-    cell    <- T.useCell emptyApp     -- global data
-    views   <- T.useFieldViews cell   -- read-only access for children
-    cursors <- T.useFieldCursors cell -- read-write access for children
-    tasks   <- R.useRef Nothing       -- storage for asynchronous tasks
+    cell    <- T.useCell emptyApp             -- global data
+    cursors <- T.useFieldCursors cell {}      -- read-write access for children
+    tasks   <- R.useRef Nothing               -- storage for asynchronous tasks
     useHashRouter Router.router cursors.route -- Install router to window
-    pure $ router { views, cursors, tasks }   -- Render router component
+    pure $ router { cursors, tasks }          -- Render router component
