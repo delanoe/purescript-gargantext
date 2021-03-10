41fb4c856b01fe533edd2ab296fb29c72ef35b8a
Przemek Kaminski <pk@intrepidus.pl>
2021-03-10 06:46:48 +0100

    [toestand] code compiles now

--- src/Gargantext/Components/App.purs
+++ src/Gargantext/Components/App.purs
@@ -11,18 +11,20 @@ import Gargantext.Hooks (useHashRouter)
 import Gargantext.Router as Router
 import Gargantext.Sessions as Sessions
 import Gargantext.Utils.Reactix as R2
+import Gargantext.Utils.Toestand as T2
 
 here :: R2.Here
 here = R2.here "Gargantext.Components.App"
 
-app :: R.Element
-app = R.createElement appCpt {} []
+app :: R2.Component ()
+app = R.createElement appCpt
 
 appCpt :: R.Component ()
 appCpt = here.component "app" cpt where
   cpt _ _ = do
     cell    <- T.useCell emptyApp             -- global data
     cursors <- T.useFieldCursors cell {}      -- read-write access for children
-    tasks   <- R.useRef Nothing               -- storage for asynchronous tasks
+    -- tasks   <- R.useRef Nothing               -- storage for asynchronous tasks
+    tasks   <- T2.useCursed Nothing           -- storage for asynchronous tasks
     useHashRouter Router.router cursors.route -- Install router to window
     pure $ router { cursors, tasks }          -- Render router component
