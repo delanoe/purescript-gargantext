29651a3348daf66bf1ae6d5bb3644592c5311b03
Przemek Kaminski <pk@intrepidus.pl>
2021-09-10 14:42:08 +0200

    [API] some fixes to enum types JSON serialization

--- src/Gargantext/Components/Forest/Tree/Node/Action/Contact/Types.purs
+++ src/Gargantext/Components/Forest/Tree/Node/Action/Contact/Types.purs
@@ -1,18 +1,25 @@
 module Gargantext.Components.Forest.Tree.Node.Action.Contact.Types where
 
 import Data.Generic.Rep (class Generic)
-import Data.Newtype (class Newtype)
 import Data.Show.Generic (genericShow)
+import Gargantext.Utils.SimpleJSON as GUSJ
 import Simple.JSON as JSON
 
 import Gargantext.Prelude (class Eq, class Show)
 
-newtype AddContactParams =
-  AddContactParams { firstname :: String, lastname :: String }
+data AddContactParams =
+    AddContactParams { firstname :: String, lastname :: String }
+  | AddContactParamsAdvanced { firstname :: String, lastname :: String }
 derive instance Eq AddContactParams
 derive instance Generic AddContactParams _
-derive instance Newtype AddContactParams _
 instance Show AddContactParams where show = genericShow
-derive newtype instance JSON.ReadForeign AddContactParams
-derive newtype instance JSON.WriteForeign AddContactParams
+instance JSON.ReadForeign AddContactParams where readImpl = GUSJ.taggedSumRep
+instance JSON.WriteForeign AddContactParams where
+  writeImpl (AddContactParams { firstname, lastname }) =
+    JSON.writeImpl { type: "AddContactParams"
+                   , values: { firstname, lastname } }
+  writeImpl (AddContactParamsAdvanced { firstname, lastname }) =
+    JSON.writeImpl { type: "AddContactParamsAdvanced"
+                   , values: { firstname, lastname } }
+
 
